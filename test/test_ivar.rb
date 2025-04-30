b75a184a7c06c0b6a6c3097c7189fc7e92c43107
Avdi Grimm <avdi@avdi.codes>
2025-04-30 11:43:25 +0000

    test from scratch too

--- test/test_ivar.rb
+++ test/test_ivar.rb
@@ -4,53 +4,5 @@ require_relative "test_helper"
 require "stringio"
 
 class TestIvar < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Ivar::VERSION
-  end
 
-  def test_ivar_tools_warns_about_unknown_ivars
-    # Capture warnings
-    original_stderr = $stderr
-    $stderr = StringIO.new
-
-    # Load the fixture file
-    require_relative "fixtures/sandwich_with_ivar_tools"
-
-    # Create an instance and call to_s to trigger the warning
-    sandwich = SandwichWithIvarTools.new
-    sandwich.to_s
-
-    # Get the captured warnings
-    warnings = $stderr.string
-
-    # Restore stderr
-    $stderr = original_stderr
-
-    # Check that the warning contains the expected text
-    assert_match(/unknown instance variable @chese/, warnings)
-    assert_match(/Did you mean: @cheese\?/, warnings)
-  end
-
-  def test_checked_ivars_warns_about_unknown_ivars
-    # Capture warnings
-    original_stderr = $stderr
-    $stderr = StringIO.new
-
-    # Load the fixture file
-    require_relative "fixtures/sandwich_with_checked_ivars"
-
-    # Create an instance and call to_s to trigger the warning
-    sandwich = SandwichWithCheckedIvars.new
-    sandwich.to_s
-
-    # Get the captured warnings
-    warnings = $stderr.string
-
-    # Restore stderr
-    $stderr = original_stderr
-
-    # Check that the warning contains the expected text
-    assert_match(/unknown instance variable @chese/, warnings)
-    assert_match(/Did you mean: @cheese\?/, warnings)
-  end
 end
