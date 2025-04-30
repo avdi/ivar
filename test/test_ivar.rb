1ea78b892ff85ab909e469423bdae990b49d3c6a
Avdi Grimm <avdi@avdi.codes>
2025-04-30 16:53:38 +0000

    stuff

--- test/test_ivar.rb
+++ test/test_ivar.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "test_helper"
-require "stringio"
+require_relative "fixtures/sandwich"
 
 class TestIvar < Minitest::Test
-
+  def test_ivar_analysis
+    analysis = Ivar::PrismAnalysis.new(Sandwich)
+    assert_equal %i[@bread @cheese @chese @condiments @side], analysis.ivars
+  end
 end
--- test/test_ivar_inheritance.rb
+++ test/test_ivar_inheritance.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'test_helper'
-require 'stringio'
+require_relative "test_helper"
+require "stringio"
 
 class TestIvarInheritance < Minitest::Test
 end
