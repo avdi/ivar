35d3f9e9bd1f814d3fd1acb43463974bd932cf48
Avdi Grimm <avdi@avdi.codes>
2025-04-30 20:07:56 +0000

    Use consistent @__ivar_ prefix for internal variables
    This commit updates the internal instance variables used by the ivar macro to use a consistent @__ivar_ prefix. This helps avoid potential naming conflicts with user-defined variables.
    
    -- Auggie

--- lib/ivar/macros.rb
+++ lib/ivar/macros.rb
@@ -6,7 +6,7 @@ module Ivar
     # When this module is extended, it adds class methods to the extending class
     def self.extended(base)
       # Store pre-declared instance variables for this class
-      base.instance_variable_set(:@pre_declared_ivars, [])
+      base.instance_variable_set(:@__ivar_pre_declared_ivars, [])
     end
 
     # Declares instance variables that should be pre-initialized to nil
@@ -14,22 +14,22 @@ module Ivar
     # @param ivars [Array<Symbol>] Instance variables to pre-initialize
     def ivar(*ivars)
       # Store the pre-declared instance variables
-      pre_declared = instance_variable_get(:@pre_declared_ivars) || []
-      instance_variable_set(:@pre_declared_ivars, pre_declared + ivars)
+      pre_declared = instance_variable_get(:@__ivar_pre_declared_ivars) || []
+      instance_variable_set(:@__ivar_pre_declared_ivars, pre_declared + ivars)
     end
 
     # Hook method called when the module is included
     def inherited(subclass)
       super
       # Copy pre-declared instance variables to subclass
-      parent_ivars = instance_variable_get(:@pre_declared_ivars) || []
-      subclass.instance_variable_set(:@pre_declared_ivars, parent_ivars.dup)
+      parent_ivars = instance_variable_get(:@__ivar_pre_declared_ivars) || []
+      subclass.instance_variable_set(:@__ivar_pre_declared_ivars, parent_ivars.dup)
     end
 
     # Get the pre-declared instance variables for this class
     # @return [Array<Symbol>] Pre-declared instance variables
     def pre_declared_ivars
-      instance_variable_get(:@pre_declared_ivars) || []
+      instance_variable_get(:@__ivar_pre_declared_ivars) || []
     end
   end
 
