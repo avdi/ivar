461589047c47fae4a905453ffc399c262e776701
Avdi Grimm <avdi@avdi.codes>
2025-04-30 17:38:05 +0000

    Refactor Ivar module methods to use def self.method_name syntax
    Changed from class << self block to individual def self.method_name definitions
    for better readability.
    
    "prefer def self.some_method to "
    
    -- Auggie

--- lib/ivar.rb
+++ lib/ivar.rb
@@ -8,16 +8,14 @@ require "did_you_mean"
 module Ivar
   @analysis_cache = {}
 
-  class << self
-    # Returns a cached analysis for the given class or module
-    # Creates a new analysis if one doesn't exist in the cache
-    def get_analysis(klass)
-      @analysis_cache[klass] ||= PrismAnalysis.new(klass)
-    end
+  # Returns a cached analysis for the given class or module
+  # Creates a new analysis if one doesn't exist in the cache
+  def self.get_analysis(klass)
+    @analysis_cache[klass] ||= PrismAnalysis.new(klass)
+  end
 
-    # For testing purposes - allows clearing the cache
-    def clear_analysis_cache
-      @analysis_cache.clear
-    end
+  # For testing purposes - allows clearing the cache
+  def self.clear_analysis_cache
+    @analysis_cache.clear
   end
 end
